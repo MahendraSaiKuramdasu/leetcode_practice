f4ef9c03cf9c295f1ccd2e5534a82a8480cf721f
mahendra sai <mahendrasaiprojects@gmail.com>
2025-03-21 16:46:06 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (91.30%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,21 +1,31 @@
 class Solution {
 public:
 
-    int TD(int i, int j, int& n, vector<vector<int>>& TRI, vector<vector<int>>& dp){
-        if(i == n-1){
-            return TRI[i][j];
+    // int TD(int i, int j, int& n, vector<vector<int>>& TRI, vector<vector<int>>& dp){
+    //     if(i == n-1){
+    //         return TRI[i][j];
+    //     }
+    //     if(dp[i][j] != -1) return dp[i][j];
+    //     dp[i][j] = TRI[i][j] + min( TD(i+1, j, n, TRI, dp) , TD(i+1, j+1, n, TRI, dp) );
+    //     return dp[i][j];
+    // }
+
+    int BU(vector<vector<int>>& dp, int& n){
+        for(int i=n-2; i>=0; i--){
+            for(int j=0; j<dp[i].size(); j++){
+                dp[i][j] = dp[i][j] + min(dp[i+1][j] , dp[i+1][j+1]);
+            }
         }
-        if(dp[i][j] != -1) return dp[i][j];
-        dp[i][j] = TRI[i][j] + min( TD(i+1, j, n, TRI, dp) , TD(i+1, j+1, n, TRI, dp) );
-        return dp[i][j];
+        return dp[0][0];
     }
 
     int minimumTotal(vector<vector<int>>& TRI) {
         int n = TRI.size();
-        vector<vector<int>> dp = TRI;
-        for(auto& i:dp){
-            fill(i.begin(), i.end(), -1);
-        }
-        return TD(0, 0, n, TRI, dp);
+        // vector<vector<int>> dp = TRI;
+        // for(auto& i:dp){
+        //     fill(i.begin(), i.end(), -1);
+        // }
+        // return TD(0, 0, n, TRI, dp);
+        return BU(TRI, n);
     }
 };
