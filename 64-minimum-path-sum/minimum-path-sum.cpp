5638ac3dae0262999983b66c8fa782d6c63ada6e
mahendra sai <mahendrasaiprojects@gmail.com>
2025-03-21 14:25:01 +0530

    Time: 0 ms (100.00%) | Memory: 16.2 MB (55.69%) - LeetSync

---
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+
+    // int dp[201][201];
+
+    // int TD(int i, int j, int& n, int& m, vector<vector<int>>& grid){
+    //     if(i>=n || j >=m) return INT_MAX;
+    //     if(i == n-1 && j == m-1)  return grid[i][j];
+    //     if(dp[i][j] != -1) return dp[i][j];
+    //     dp[i][j] = grid[i][j] + min(TD(i+1, j, n, m, grid) , TD(i, j+1, n, m, grid));
+    //     return dp[i][j];
+    // }
+
+    int BU(int n, int m, vector<vector<int>>& grid){
+        vector<vector<int>> dp(n+1, vector<int>(m+1, INT_MAX));
+        dp[n-1][m-1] = grid[n-1][m-1];
+        for(int i=n-1; i>=0; i--){
+            for(int j=m-1; j>=0; j--){
+                if(i == n-1 && j == m-1) continue;
+                dp[i][j] = grid[i][j] + min(dp[i+1][j] , dp[i][j+1]);
+            }
+        }
+        return dp[0][0];
+    }
+
+    int minPathSum(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        // memset(dp, -1, sizeof(dp));
+        // return TD(0, 0, n, m, grid);
+        return BU(n, m, grid);
+    }
+};
