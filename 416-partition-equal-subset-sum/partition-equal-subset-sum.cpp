09ac619c85827df41955641259bca5fd21ffbb2d
mahendra sai <mahendrasaiprojects@gmail.com>
2025-03-22 14:45:57 +0530

    Time: 125 ms (53.66%) | Memory: 184.4 MB (11.34%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,17 +1,36 @@
 class Solution {
 public:
 
-    int dp[201][20001];
+    // int dp[201][20001];
 
-    int TD(int n, int sum, vector<int>& nums){
-        if(n < 0) return 0;
-        if(sum < 0) return 0;
-        if(sum == nums[n]) return 1;
-        if(dp[n][sum] != -1) return dp[n][sum];
-        int exclude = TD(n-1, sum, nums);
-        int include = TD(n-1, sum-nums[n], nums);
-        dp[n][sum] = (exclude || include);
-        return dp[n][sum];
+    // int TD(int n, int sum, vector<int>& nums){
+    //     if(n < 0) return 0;
+    //     if(sum < 0) return 0;
+    //     if(sum == nums[n]) return 1;
+    //     if(dp[n][sum] != -1) return dp[n][sum];
+    //     int exclude = TD(n-1, sum, nums);
+    //     int include = TD(n-1, sum-nums[n], nums);
+    //     dp[n][sum] = (exclude || include);
+    //     return dp[n][sum];
+    // }
+
+    bool BU(int n, int sum, vector<int>& nums){
+        vector<vector<int>> dp(n, vector<int>(sum*2+1, 0));
+        for(int i=0; i<n; i++){
+            dp[i][0] = 1;
+        }
+        dp[0][nums[0]] = 1;
+        for(int i=1; i<n; i++){
+            for(int j=0; j<=sum; j++){
+                int exclude = dp[i-1][j];
+                int include = 0;
+                if(j - nums[i] >= 0){
+                    include = dp[i-1][j - nums[i]];
+                }
+                dp[i][j] = include || exclude;
+            }
+        }
+        return dp[n-1][sum];
     }
 
     bool canPartition(vector<int>& nums) {
@@ -22,7 +41,8 @@ public:
         }
         if(sum%2 == 1) return false;
         sum = sum/2;
-        memset(dp, -1, sizeof(dp));
-        return TD(n-1, sum, nums);
+        // memset(dp, -1, sizeof(dp));
+        // return TD(n-1, sum, nums);
+        return BU(n, sum, nums);
     }
 };
