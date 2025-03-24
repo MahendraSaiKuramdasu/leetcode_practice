1a00c808188af496161cd4073df75f8314350234
mahendra sai <mahendrasaiprojects@gmail.com>
2025-03-24 14:48:31 +0530

    Time: 370 ms (80.46%) | Memory: 73.4 MB (68.02%) - LeetSync

--- 2162-partition-array-into-two-arrays-to-minimize-sum-difference/partition-array-into-two-arrays-to-minimize-sum-difference.cpp
+++ 2162-partition-array-into-two-arrays-to-minimize-sum-difference/partition-array-into-two-arrays-to-minimize-sum-difference.cpp
@@ -3,41 +3,42 @@ public:
 
     int minimumDifference(vector<int>& nums) {
         int N = nums.size();
-        long long sum = accumulate(nums.begin(), nums.end(), 0LL);
-        int n = N / 2;
-        vector<vector<int>> left(n + 1), right(n + 1);
-
-        for (int mask = 0; mask < (1 << n); mask++) {
-            int sz = 0, lt = 0, rt = 0;
-            for (int i = 0; i < n; i++) {
-                if (mask & (1 << i)) {
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        // sort(nums.begin(), nums.end());
+        int n = N/2;
+        vector<vector<int>> left(n+1), right(n+1);
+        for(int maxi=0; maxi<(1<<n); maxi++){
+            int sz = 0;
+            int lt = 0;
+            int rt = 0;
+            for(int i=0; i<n; i++){
+                if(maxi & (1<<i)){
                     sz++;
-                    lt += nums[i];
-                    rt += nums[i + n];
+                    lt+=nums[i];
+                    rt+=nums[i+n];
                 }
             }
             left[sz].push_back(lt);
             right[sz].push_back(rt);
         }
-
-        for (int i = 1; i < n; i++) {
+        for(int i=1; i<n; i++){
             sort(right[i].begin(), right[i].end());
         }
-
-        long long mini = min(abs(sum - 2*(left[n][0])), abs(sum - 2*(right[n][0])));
-        for (int i = 1; i < n; i++) {
-            for (auto& a : left[i]) {
-                auto itr = lower_bound(right[n - i].begin(), right[n - i].end(), (sum / 2) - a);
-                if (itr != right[n - i].end()) {
-                    mini = min(mini, abs(sum - 2 * (a + *itr)));
+        int mini = min(abs(sum - 2*left[n][0]), abs(sum - 2*right[n][0]));
+        // int mini = INT_MAX;
+        for(int i=1; i<n; i++){
+            for(auto& a : left[i]){
+                int b = sum/2 - a;
+                auto itr = lower_bound(right[n-i].begin(), right[n-i].end(), b);
+                if(itr != right[n-i].end()){
+                    mini = min(mini, abs(sum - 2*(a+(*itr))));
                 }
-                if (itr != right[n - i].begin()) {
+                if(itr != right[n-i].begin()){
                     --itr;
-                    mini = min(mini, abs(sum - 2 * (a + *itr)));
+                    mini = min(mini, abs(sum - 2*(a+(*itr))));
                 }
             }
         }
-
-        return static_cast<int>(mini);
+        return mini;
     }
 };
