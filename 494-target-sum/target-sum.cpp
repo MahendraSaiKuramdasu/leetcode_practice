536a39dd22580afb6bae97751475b99dfa254c80
mahendra sai <mahendrasaiprojects@gmail.com>
2025-03-26 19:24:49 +0530

    Time: 14 ms (58.15%) | Memory: 11.9 MB (69.29%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -3,22 +3,34 @@ public:
 
     int dp[20][4001];
 
-    int TD(int n, int target, vector<int>& nums){
-        if(n == 0){
-            if(target == 0 && nums[0] == 0) return 2;
-            return abs(target) == nums[0];
+    // int TD(int n, int target, vector<int>& nums){
+    //     if(n == 0){
+    //         if(target == 0 && nums[0] == 0) return 2;
+    //         return abs(target) == nums[0];
+    //     }
+    //     if(dp[n][target + 2000] != -1) return dp[n][target + 2000];
+    //     int add = TD(n-1, target+nums[n], nums);
+    //     int sub = TD(n-1, target-nums[n], nums);
+    //     return dp[n][target + 2000] = (add + sub);
+    // }
+
+    int BU(int n, int target, vector<int>& nums){
+        dp[0][2000 - nums[0]] += 1; 
+        dp[0][2000 + nums[0]] += 1;
+        for(int i=1; i<n; i++){
+            for(int j=-1000; j<=1000; j++){
+                int add = dp[i-1][2000 + (j + nums[i])];
+                int sub = dp[i-1][2000 + (j - nums[i])];
+                dp[i][2000 + j] = (add + sub);
+            }
         }
-        if(dp[n][target + 2000] != -1) return dp[n][target + 2000];
-        int add = TD(n-1, target+nums[n], nums);
-        int sub = TD(n-1, target-nums[n], nums);
-        return dp[n][target + 2000] = (add + sub);
+        return dp[n-1][2000 + target]; 
     }
-
-
 
     int findTargetSumWays(vector<int>& nums, int target) {
         int n = nums.size();
-        memset(dp, -1, sizeof(dp));
-        return TD(n-1, target, nums);
+        memset(dp, 0, sizeof(dp));
+        // return TD(n-1, target, nums);
+        return BU(n, target, nums);
     }
 };
