88a3c1977947f406d71ffd49209f3164783d6cc9
mahendra sai <mahendrasaiprojects@gmail.com>
2025-03-26 18:36:05 +0530

    Time: 34 ms (22.39%) | Memory: 46.1 MB (14.03%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,26 +1,58 @@
+#define ll long long
+int mod = 1e11+7 ;
+
 class Solution {
 public:
 
-    int TD(int n, int tar, vector<int>& coins, vector<vector<int>>& dp){
-        if(n < 0) return 0;
-        if(tar < 0) return 0;
-        if(tar == 0){
-            return 1;
+    // int TD(int n, int tar, vector<int>& coins, vector<vector<int>>& dp){
+        // if(n == 0){
+        //     if(tar%coins[0] == 0){
+        //         return 1;
+        //     }
+        //     return 0;
+        // }
+    //     if(tar < 0) return 0;
+    //     if(tar == 0){
+    //         return 1;
+    //     }
+    //     if(dp[n][tar] != -1){
+    //         return dp[n][tar];
+    //     }
+    //     int exclude = TD(n-1, tar, coins, dp);
+    //     int include = 0;
+    //     if(tar - coins[n] >= 0){
+    //         include = TD(n, tar-coins[n], coins, dp);
+    //     }
+    //     dp[n][tar] = include + exclude;
+    //     return dp[n][tar];
+    // }
+
+    int BU(int n, int tar, vector<int>& coins){
+        vector<vector<ll>> dp(n, vector<ll>(tar+1, 0));
+        for(int i=0; i<n; i++){
+            dp[i][0] = 1;
         }
-        if(dp[n][tar] != -1){
-            return dp[n][tar];
+        for(int i=1; i<=tar; i++){
+            if(i%coins[0] == 0){
+                dp[0][i] = 1;
+            }
         }
-        int exclude = TD(n-1, tar, coins, dp);
-        int include = 0;
-        if(tar - coins[n] >= 0){
-            include = TD(n, tar-coins[n], coins, dp);
+        for(int i=1; i<n; i++){
+            for(int j=0; j<=tar; j++){
+                ll exclude = dp[i-1][j];
+                ll include = 0;
+                if(j - coins[i] >= 0){
+                    include = dp[i][j-coins[i]];
+                }
+                dp[i][j] = (include + exclude)%mod;
+            }
         }
-        dp[n][tar] = include + exclude;
-        return dp[n][tar];
+        return dp[n-1][tar];
     }
 
     int change(int amount, vector<int>& coins) {
-        vector<vector<int>> dp(coins.size(), vector<int>(amount+1, -1));
-        return TD(coins.size()-1, amount, coins, dp);
+        // vector<vector<int>> dp(coins.size(), vector<int>(amount+1, -1));
+        // return TD(coins.size()-1, amount, coins, dp);
+        return BU(coins.size(), amount, coins);
     }
 };
