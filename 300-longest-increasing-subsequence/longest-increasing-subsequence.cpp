fa33725ffa018e437fa77d7b8dec90ce46f65640
mahendra sai <mahendrasaiprojects@gmail.com>
2025-04-09 13:59:52 +0530

    Time: 310 ms (25.86%) | Memory: 41.6 MB (32.33%) - LeetSync

---
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+
+    int dp[2502][2501];
+
+    int TD(int p, int i, int n, vector<int>& nums){
+        if(i == n) return 0;
+        if(dp[p+1][i] != -1) return dp[p+1][i];
+        int ans = 0;
+        if(p == -1 || nums[p] < nums[i]){
+            ans = 1 + TD(i, i+1, n, nums);
+        }
+        ans = max(ans, TD(p, i+1, n, nums));
+        return dp[p+1][i] = ans; 
+    }
+
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        memset(dp, -1, sizeof(dp));
+        int ans = TD(-1, 0, n, nums);
+        return ans;
+    }
+};
