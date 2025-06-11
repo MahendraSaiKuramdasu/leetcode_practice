50ec63d399ccda825ccd7cd3aed8c81271ed8935
mahendra sai <mahendrasaiprojects@gmail.com>
2025-06-11 14:56:06 +0530

    Time: 17 ms (43.32%) | Memory: 37.5 MB (23.49%) - LeetSync

--- 334-increasing-triplet-subsequence/increasing-triplet-subsequence.py
+++ 334-increasing-triplet-subsequence/increasing-triplet-subsequence.py
@@ -1,17 +1,13 @@
 class Solution:
     def increasingTriplet(self, nums: List[int]) -> bool:
-        n = len(nums)
-        least = [0] * n
-        mini = 2**31
-        maxi = -2**31
-        for i in range(0, n):
-            least[i] = mini
-            mini = min(mini, nums[i])
-
-        for i in range(n-1, -1, -1):
-            if least[i] < nums[i] and nums[i] < maxi:
+        first = 2**31
+        second = 2**31
+        for i in nums:
+            if i <= first:
+                first = i
+            elif i <= second:
+                second = i
+            else:
                 return True
-            
-            maxi = max(maxi, nums[i])
 
         return False
