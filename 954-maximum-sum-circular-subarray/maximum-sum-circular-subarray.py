64c0899da4ebd5ed718e368180ef317d892f58db
mahendra sai <mahendrasaiprojects@gmail.com>
2025-06-14 22:29:45 +0530

    Time: 65 ms (91.44%) | Memory: 21.3 MB (52.49%) - LeetSync

--- 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.py
+++ 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.py
@@ -2,26 +2,27 @@ class Solution:
     def maxSubarraySumCircular(self, arr: List[int]) -> int:
 
         n = len(arr)
-        prefix = [0] * (n+1)
-        maxi = -2**31
+        mini = 2**31
         k = 0
-        max_prefix = -2**31
+        maxi = -2**31
+        mk = 0
         s = 0
         for i in range(0, n):
             s += arr[i]
             k += arr[i]
-            maxi = max(maxi, k)
-            max_prefix = max(max_prefix, s)
-            if k < 0:
+            mk += arr[i]
+            maxi = max(maxi, mk)
+            mini = min(mini, k)
+            if k > 0:
                 k = 0
-            prefix[i+1] = max_prefix
-        
-        s = 0
-          
-        for i in range(n-1, -1, -1):
-            s += arr[i]
-            maxi = max(maxi, (s+prefix[i]))
-            
-        return maxi
+            if mk < 0:
+                mk = 0
+
+        if s == mini:
+            return maxi
+
+        ans = s - mini
+
+        return max(ans, maxi)
 
         
