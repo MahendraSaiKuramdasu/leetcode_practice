8e7d72de81ad0c60cd8debf7be81713a51223a57
mahendra sai <mahendrasaiprojects@gmail.com>
2025-07-09 13:47:21 +0530

    Time: 71 ms (5.08%) | Memory: 18.3 MB (7.67%) - LeetSync

--- 139-word-break/word-break.py
+++ 139-word-break/word-break.py
@@ -1,6 +1,4 @@
 def DP(i, j, s, st, dp):
-    # if i > j:
-    #     return False
     if s[i:j+1] in st:
         return True
 
@@ -17,10 +15,35 @@ def DP(i, j, s, st, dp):
     dp[i][j] = False
     return False
 
+
+def Tab(n, s, st):
+    dp = [[False for _ in range(n)] for _ in range(n)]
+
+    for i in range(0, n):
+        if s[i] in st:
+            dp[n-1][i] = True
+
+    for i in range(n-2, -1, -1):
+        for j in range(0, n):
+            if i <= j:
+                if s[i:j+1] in st:
+                    dp[i][j] = True
+                else:
+                    for k in range(i,j):
+                        left = dp[i][k]
+                        right = dp[k+1][j]
+                        if left == True and right == True:
+                            dp[i][j] = True
+                            break
+
+    print(dp)
+    return dp[0][n-1]
+
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         n = len(s)
         st = set(wordDict)
-        dp = [[-1 for _ in range(n+1)] for _ in range(n+1)]
-        ans = DP(0, n-1, s, st, dp)
+        # dp = [[-1 for _ in range(n+1)] for _ in range(n+1)]
+        # ans = DP(0, n-1, s, st, dp)
+        ans = Tab(n, s, st)
         return ans
