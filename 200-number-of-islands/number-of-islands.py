2fac3b121bf189fdd0df92f96cd80b5ab7e2cbd8
mahendra sai <mahendrasaiprojects@gmail.com>
2025-08-09 00:27:34 +0530

    Time: 233 ms (84.93%) | Memory: 20.1 MB (68.33%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,18 +1,18 @@
 
 dir = [[0,1],[0,-1],[1,0],[-1,0]]
 
-def check(i, j, n, m, grid, visited):
-    if i>=0 and i < n and j >= 0 and j < m and grid[i][j] == "1" and visited[i][j] == 0:
+def check(i, j, n, m, grid):
+    if i>=0 and i < n and j >= 0 and j < m and grid[i][j] == "1":
         return True
     return False
 
-def dfs(i, j, n, m, grid, visited):
-    visited[i][j] = 1
+def dfs(i, j, n, m, grid):
+    grid[i][j] = "0"
     for d in dir:
         x = i + d[0]
         y = j + d[1]
-        if check(x, y, n, m, grid, visited):
-            dfs(x, y, n, m, grid, visited)
+        if check(x, y, n, m, grid):
+            dfs(x, y, n, m, grid)
 
 
 class Solution:
@@ -21,12 +21,12 @@ class Solution:
         m = len(grid[0])
         island_cnt = 0
         # visited matrix
-        visited = [[0 for _ in range(m)] for _ in range(n)]
+        # visited = [[0 for _ in range(m)] for _ in range(n)]
         # iteration of matrix
         for i in range(0, n):
             for j in range(0, m):
-                if grid[i][j] == "1" and visited[i][j] == 0:
-                    dfs(i, j, n, m, grid, visited)
+                if grid[i][j] == "1":
+                    dfs(i, j, n, m, grid)
                     island_cnt +=1
 
         return island_cnt
