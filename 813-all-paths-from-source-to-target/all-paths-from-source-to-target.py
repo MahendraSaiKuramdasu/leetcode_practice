41334b4e2321e06b455747698954b49bb0e9aa34
mahendra sai <mahendrasaiprojects@gmail.com>
2025-08-09 23:25:33 +0530

    Time: 8 ms (15.87%) | Memory: 19 MB (78.00%) - LeetSync

--- 813-all-paths-from-source-to-target/all-paths-from-source-to-target.py
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.py
@@ -1,4 +1,4 @@
-def dfs(i, n, graph, vis, carry, res):
+def dfs(i, n, graph, carry, res):
     # vis[i] = 1
     carry = carry.copy()
     carry.append(i)
@@ -6,17 +6,17 @@ def dfs(i, n, graph, vis, carry, res):
         res.append(carry)
     for j in graph[i]:
         # if vis[j] != 1:
-            dfs(j, n, graph, vis, carry, res)
+            dfs(j, n, graph, carry, res)
     # vis[i] = 0
 
 
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
         n = len(graph)
-        vis = [0] * n
+        # vis = [0] * n
         res = []
         carry = []
-        dfs(0, n-1, graph, vis, carry, res)
+        dfs(0, n-1, graph, carry, res)
         return res
 
         
