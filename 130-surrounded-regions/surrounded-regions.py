6ff048580807d20352128c49d00c3db6d009fe98
mahendra sai <mahendrasaiprojects@gmail.com>
2025-08-11 00:41:13 +0530

    Time: 5 ms (65.96%) | Memory: 22.1 MB (24.70%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.py
+++ 130-surrounded-regions/surrounded-regions.py
@@ -11,24 +11,13 @@ def boundary_check(i, j, n, m):
         return True
     return False
 
-def change_dfs(i, j, n, m, board, vis):
-    board[i][j] = "X"
-    vis[i][j] = 0
+def dfs(i, j, n, m, board, vis):
+    vis[i][j] = "O"
     for d in dir:
         x = i + d[0]
         y = j + d[1]
-        if not_out_of_bound(x, y, n, m) and vis[x][y] == 1:
-            change_dfs(x, y, n, m, board, vis)
-
-def dfs(i, j, n, m, board, vis, flag):
-    vis[i][j] = 1
-    if boundary_check(i, j, n, m):
-        flag[0] = False 
-    for d in dir:
-        x = i + d[0]
-        y = j + d[1]
-        if not_out_of_bound(x, y, n, m) and board[x][y] != "X" and vis[x][y] != 1:
-            dfs(x, y, n, m, board, vis, flag)
+        if not_out_of_bound(x, y, n, m) and board[x][y] == "O" and vis[x][y] == "X":
+            dfs(x, y, n, m, board, vis)
 
 class Solution:
     def solve(self, board: List[List[str]]) -> None:
@@ -37,17 +26,16 @@ class Solution:
         """
         n = len(board)
         m = len(board[0])
-        vis = [[0 for _ in range(m)]for _ in range(0, n)]
-        change = []
+        vis = [["X" for _ in range(0, m)] for _ in range(0, n)]
         for i in range(0, n):
             for j in range(0, m):
-                if board[i][j] == "O" and vis[i][j] == 0:
-                    flag = [True]
-                    dfs(i, j, n, m, board, vis, flag)
-                    if flag[0] == True:
-                        change.append([i,j])
-        for i in change:
-            change_dfs(i[0], i[1], n, m, board, vis)
+                if boundary_check(i, j, n, m) and board[i][j] == "O" and vis[i][j]            ==  "X":
+                    dfs(i, j, n, m, board, vis)
+
+        for i in range(0, n):
+            for j in range(0, m):
+                board[i][j] = vis[i][j]
+
 
 
         
